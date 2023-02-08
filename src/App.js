c5425da13de5825f3e7c6e10190588d7144ab369
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-08 15:32:44 +0100

    Primer push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+<h1 className="text-10xl font-bold underline">
+      Hello world!
+    </h1>
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '0px'
+      },
+      fontFamily: {
+
+      },
+      backgroundImage: {
+
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2F7A78",
+
+          "secondary": "#41AFA9",
+
+          "accent": "#DFF2F1",
+
+          "neutral": "#0E0E0E",
+
+          "base-100": "#17252A",
+
+          "info": "#FEFFFF",
+
+          "success": "#187C54",
+
+          "warning": "#E7BC0D",
+
+          "error": "#FB1820",
+
+          "fuerte": "#17252A",
+
+
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
